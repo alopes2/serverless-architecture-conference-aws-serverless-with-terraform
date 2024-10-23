73bc27474392c4ee23ad642caa437f08f9508fba
Andre Lopes <andrevitorlopes@gmail.com>
2024-10-23 15:01:01 +0200

    Add create movie

--- iac/lambdas.tf
+++ iac/lambdas.tf
@@ -4,3 +4,10 @@ module "get_movie_lambda" {
   source_file_path = "./lambda_init_code/index.mjs"
   policies         = [data.aws_iam_policy_document.get_movie_item.json]
 }
+
+module "create_movie_lambda" {
+  source           = "./modules/lambda"
+  name             = "create-movie"
+  source_file_path = "./lambda_init_code/index.mjs"
+  policies         = [data.aws_iam_policy_document.create_movie_item.json]
+}
